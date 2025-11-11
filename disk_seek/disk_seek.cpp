41c5555f903f69c1f6007f3cb1441a73b1e06e36
cherr <charity.grey1@gmail.com>
2025-11-11 11:44:55 -0800

    read command line arguments

--- disk_seek/disk_seek.cpp
+++ disk_seek/disk_seek.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#define TOTAL_CYLINDERS 5000
+#define TOTAL_PLATTERS 10
+#define IS_DOUBLE_SIDED true
 
-int main()
+
+
+
+int main(int argc, char **argv)
 {
-    std::cout << "Here is some text.";
+    std::cout << argc << " arguments provided.\n";
+    for (int i = 0; i < argc; ++i)
+    {
+        std::cout << "Argument " << i << ": " << argv[i] << "\n";
+    }
     return 0;
 }
