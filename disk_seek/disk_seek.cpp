d6dc5a1d1b0690c4649343b59967f1149a120ba3
cherr <charity.grey1@gmail.com>
2025-11-11 14:50:05 -0800

    add compilation instructions

--- disk_seek/disk_seek.cpp
+++ disk_seek/disk_seek.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
+#include <string>
 #define TOTAL_CYLINDERS 5000
 #define TOTAL_PLATTERS 10 //[[maybe unused]]
 #define IS_DOUBLE_SIDED true //[[maybe unused]]
 
 
-int * parse_disk_seek_requests(char **argv)
-{
-    // Placeholder for actual validation logic
-    return nullptr;
+int * parse_disk_seek_requests(int argc, char **argv) {
+    int * requests = new int[argc - 1];
+    for (int i = 1; i < argc; ++i) {
+        int request {};
+        try {
+            request = std::stoi(argv[i]);
+        } catch (const std::exception& e) {
+            delete[] requests;
+            return nullptr;
+        }
+
+        if (request < 0 || request >= TOTAL_CYLINDERS) {
+            delete[] requests;
+            return nullptr;
+        } else {
+            requests[i - 1] = request;
+        }
+    }
+    return requests;
 
 }
 
 int main(int argc, char **argv)
 {
-    int * requests { parse_disk_seek_requests(argv) };
+    int * requests { parse_disk_seek_requests(argc, argv) };
     if (!requests)
     {
         std::cerr << "Invalid disk seek inputs provided. Must be list of number within " << TOTAL_CYLINDERS << " \n";
